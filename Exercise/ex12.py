868b12fdabefcd95a41e61c970320f23e3fcfe2c
kasib <kasibzxc@gmail.com>
2023-11-08 11:20:13 -0500

    add case for non integer

--- Exercise/ex12.py
+++ Exercise/ex12.py
@@ -1,13 +1,21 @@
 
 def main():
-    choice = int(input("Enter integer: "))
-    if choice > 0:
+    choice = input("Enter integer: ")
+
+    try:
+        choice = int(choice)
+    except:
+        print(f"{choice} is not an integer.")
+        return
+
+    choice = int(choice)
+    if (choice) > 0:
         choice = 0 - choice
     elif choice == 0:
         choice = 0
     else:
         choice = 0 - choice
-    
+
     print(choice)
 
 if __name__ == "__main__":
